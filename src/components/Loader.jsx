8cf3a2df7f8b50ca5fd9764ef72dfc0859a8100a
Bikash Kakati <bikashkakati796@gmail.com>
2024-04-08 20:49:45 +0530

    Fixbug- Fixing the class spelling

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loader = () => {
     return (
         <div className='h-dvh w-dvh flex items-center justify-center'>
-            <div class="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
+            <div className="animate-spin inline-block size-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500" role="status" aria-label="loading">
             </div>
         </div>
     )
