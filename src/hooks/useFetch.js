5d189f3e3aec3e69e6e108199061e0493d7cd82d
Bikash Kakati <bikashkakati796@gmail.com>
2024-04-15 07:51:10 +0530

    Feature- Made a useFetch hook
    Now we have usefetch hook to call the api which gives fetching data, loading state and error aswell

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+const BASE_URL = "https://hoblist.com/api/movieList";
+
+
+export function useFetch() {
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        console.log("running");
+        setLoading(true);
+        fetchDataFromApi()
+            .then(res => {
+                setData(res?.result || [])
+                setLoading(false);
+            })
+            .catch(err => {
+                setError(err);
+                setLoading(false);
+            })
+    }, [])
+
+    return { data, loading, error }
+}
+
+async function fetchDataFromApi() {
+    try {
+        const params = {
+            category: 'movies',
+            language: 'kannada',
+            genre: 'all',
+            sort: 'voting',
+        }
+        const {data} = await axios.post(BASE_URL, params)
+        return data;
+    } catch (err) {
+        return err
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,36 +1,11 @@
-import React, { useEffect } from 'react'
-import Card from '../components/Card'
-import axios from 'axios'
-import { useState } from 'react';
+import React from 'react';
+import Card from '../components/Card';
 import Loader from "../components/Loader";
-const BASE_URL = "https://hoblist.com/api/movieList";
+import { useFetch } from '../hooks/useFetch';
+
 
 const Home = () => {
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    fetchDataFromApi();
-  }, [])
-
-  async function fetchDataFromApi() {
-    setLoading(true);
-    try {
-      const params = {
-        category: 'movies',
-        language: 'kannada',
-        genre: 'all',
-        sort: 'voting',
-      };
-      const { data } = await axios.post(BASE_URL, params)
-      setData(data?.result || []);
-      setLoading(false);
-    } catch (err) {
-      setError(err.message);
-    }
-  }
-
+  const {data,loading, error} = useFetch();
 
   return (
     <div className='w-full pt-10 flex items-center justify-center flex-wrap'>
